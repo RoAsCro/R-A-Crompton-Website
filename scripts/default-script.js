4a0bb18a83a47304075a7a17e841c6aac24708dc
Roland Crompton <rollycrompton@gmail.com>
2023-10-31 14:55:07 +0000

    Adde dark/light mode support to all pages
    Signed-off-by: Roland Crompton <rollycrompton@gmail.com>

--- scripts/default-script.js
+++ scripts/default-script.js
@@ -1,18 +1,37 @@
 function colourMode() {
     let button = document.getElementById("mode");
-    let sheet = "./styles/default-style.css";
+    let sheet = "/default-style.css";
+    let style = document.getElementById("style-sheet");
+    let path = style.getAttribute("href");
+    path = path.substring(0, path.lastIndexOf("/"));
     let mode = "dark"
+    let text = "Dark"
+    
 
     if (button.getAttribute("value") === "dark") {
-        sheet = "./styles/dark-mode.css";
+        sheet = "/dark-mode.css";
         mode = "light";
+        text = "Light";
     }
     button.setAttribute("value", mode);
-    document.getElementById("style-sheet").setAttribute("href", sheet);
+    button.innerHTML = text + " Mode";
+    document.getElementById("style-sheet").setAttribute("href", path + sheet);
 
+}
+
+function loadUp() {
+
+    // Add the dark mode button to the top of the document
+    const button = document.createElement("button");
+    button.setAttribute("id", "mode");
+    button.innerHTML = "Dark Mode";
+    const body = document.getElementsByTagName("body")[0];
+
+    body.insertBefore(button, body.firstChild);
 }
 
 
 window.onload = function() {
+    loadUp();
     document.getElementById("mode").onclick = colourMode;
 }
