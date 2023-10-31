248a229371436bfe40163c41b30088afa5a9c72b
Roland Crompton <rollycrompton@gmail.com>
2023-10-31 17:18:51 +0000

    Test10

--- scripts/gallery-script.js
+++ scripts/gallery-script.js
@@ -4,20 +4,20 @@ window.onload = function() {
     const filePath = "/resources/images/gallery-images"
     
     let gallery = document.getElementsByTagName("head")[0].getAttribute("id");
-    window.alert("t9");
+    window.alert("t10");
 
-    $.get("scripts\images.xml", function( data ) {
+    $.get("../scripts/images.xml", function( data ) {
         window.alert("Two");
 
-        const images = data.querySelectorAll("image < " + gallery);
+        // const images = data.querySelectorAll("image < " + gallery);
 
-        let output = "<p>";
-        for (let i = 0; i < images.length; i++) {
-            const img = document.createElement("img");
-            img.setAttribute("src", filePath + images[i].querySelector("name").innerHTML);
-            img.setAttribute("alt", images[i].querySelector("alt").innerHTML)
-            document.getElementsByTagName("body")[0].append;
-        }
+        // let output = "<p>";
+        // for (let i = 0; i < images.length; i++) {
+        //     const img = document.createElement("img");
+        //     img.setAttribute("src", filePath + images[i].querySelector("name").innerHTML);
+        //     img.setAttribute("alt", images[i].querySelector("alt").innerHTML)
+        //     document.getElementsByTagName("body")[0].append;
+        // }
         },
         "xml"
         )
